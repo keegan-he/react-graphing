f57378f76cdc84c41ad8f8244b58f5b700d8669a
Keegan H <keeganslane@gmail.com>
2019-04-21 13:08:33 -0700

    reformatting

--- src/App.js
+++ src/App.js
@@ -6,32 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React - Hi Keegan
-          </a>
-          
-        </header>
-
-
-    <div class="Container">
-        <div class="Nav">coin price</div>
-        <div class="Box">
+    <div className="Container">
+        <div className="Nav">coin price</div>
+        <div className="Box">
             <div id="GraphBox">
             </div>
         </div>
-        <div class="Footer">
+        <div className="Footer">
         </div>
-        <div class="Coins">
+        <div className="Coins">
             <div>BTC</div>
             <div>ETH</div>
             <div>LTC</div>
@@ -40,9 +23,9 @@ class App extends Component {
         </div>
         <div id="PriceBox">
         </div>
-        <div class="BottomFoot">
+        <div className="BottomFoot">
         </div>
-        <div class="Button">
+        <div className="Button">
             <button onClick="doFetch()">Update Coin Data</button>
         </div>
         <section id="output">

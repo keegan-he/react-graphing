e602c0f1c3273cecccab75f17a044b5821f31c95
Keegan H <keeganslane@gmail.com>
2019-04-27 03:15:02 -0700

    format fix

--- src/App.js
+++ src/App.js
@@ -4,79 +4,82 @@ import './App.css';
 class App extends Component {
 
 
-state = {
-  btc: '',
-  bch: '',
-  eth: '',
-  ltc: '',
-  bnb: '',
-};
+  state = {
+    btc: '',
+    bch: '',
+    eth: '',
+    ltc: '',
+    bnb: '',
+  };
 
 
-componentDidMount() {
-  fetch("https://api.coinlore.com/api/tickers/")
-  .then(response => response.json())
-  .then(data => {
-    console.log('fetched coin data', data);
-    this.setState({
-      btc: data.data[0].price_usd,
-      bch: data.data[3].price_usd,
-      eth: data.data[1].price_usd,
-      ltc: data.data[4].price_usd,
-      bnb: data.data[6].price_usd,
-    });
-  });
-}
+  componentDidMount() {
+    fetch("https://api.coinlore.com/api/tickers/")
+      .then(response => response.json())
+      .then(data => {
+        console.log('fetched coin data', data);
+        this.setState({
+          btc: data.data[0].price_usd,
+          bch: data.data[3].price_usd,
+          eth: data.data[1].price_usd,
+          ltc: data.data[4].price_usd,
+          bnb: data.data[6].price_usd,
+        });
+      });
+  }
 
-doFetch = () => {
-  console.log('onRefresh method running')
-  fetch("https://api.coinlore.com/api/tickers/")
-  .then(response => response.json())
-  .then(data => {
-    console.log('fetched coin data', data);
-    this.setState({
-      btc: data.data[0].price_usd,
-      bch: data.data[3].price_usd,
-      eth: data.data[1].price_usd,
-      ltc: data.data[4].price_usd,
-      bnb: data.data[6].price_usd,
-    });
+  doFetch = () => {
+    console.log('onRefresh method running')
+    fetch("https://api.coinlore.com/api/tickers/")
+      .then(response => response.json())
+      .then(data => {
+        console.log('fetched coin data', data);
+        this.setState({
+          btc: data.data[0].price_usd,
+          bch: data.data[3].price_usd,
+          eth: data.data[1].price_usd,
+          ltc: data.data[4].price_usd,
+          bnb: data.data[6].price_usd,
+        });
 
-  });
+      });
 
-}
+  }
 
   render() {
     console.log("render function is running")
     return (
       <div className="App">
-    <div className="Container">
-        <div className="Nav">coin price</div>
-        <div className="Box">
-            <div id="GraphBox">
+        <div className="Container">
+          <div className="Nav">coin price</div>
+          <div className="Box">
+
+            <div className="GraphBox">
+
             </div>
-        </div>
-        <div className="Footer">
-        </div>
-        <div className="Coins">
+
+          </div>
+          <div className="Footer">
+          </div>
+          <div className="Coins">
             <div>BTC price: {this.state.btc}</div>
             <div>ETH price: {this.state.eth}</div>
             <div>LTC price: {this.state.ltc}</div>
             <div>BCH price: {this.state.bch}</div>
             <div>BNB price: {this.state.bnb}</div>
-        </div>
-        <div id="PriceBox">
-        </div>
-        <div className="BottomFoot">
-        </div>
-        <div className="Button">
+          </div>
+          <div id="PriceBox">
+          </div>
+          <div className="BottomFoot">
+          </div>
+          <div className="Button">
             <button onClick={() => this.doFetch()}> Update Coin Data </button>
+          </div>
+          <section id="output">
+          </section>
         </div>
-        <section id="output">
-        </section>
-    </div>
 
-    <script src="main.js"></script>
+        <script src="main.js"></script>
 
       </div>
     );

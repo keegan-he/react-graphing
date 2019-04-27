aaec5d27ecd178548dfd6ca36c50e601e4bfb2fa
Keegan H <keeganslane@gmail.com>
2019-04-26 23:31:35 -0700

    pulling btc data through

--- src/App.js
+++ src/App.js
@@ -14,15 +14,41 @@ state = {
 };
 
 
-componentDidMount() {
+// componentDidMount() {
+//   fetch("https://api.coinlore.com/api/tickers/")
+//     .then(response => response.formData())
+//     .then(data => {
+//       console.log("got data", data);
+//     });
+// }
+
+doFetch = () => {
+  console.log('onRefresh method running')
   fetch("https://api.coinlore.com/api/tickers/")
-    .then(response => response.formData())
-    .then(data => {
-      console.log("got data", data);
-      this.setState({
-        apiData:data,
-      });
+  .then(response => response.json())
+  .then(data => {
+    console.log('fetched coin data', data);
+    // this.setState({
+    //   searchBox: 'Oakland, California',
+    //   location: data.name,
+    //   temperature: Math.round(data.main.temp - 273),
+    //   description: data.weather[0].main,
+    //   windSpeed: Math.round(data.wind.speed),
+    //   humidity: Math.round(data.main.humidity),
+    //   pressure: Math.round(data.main.pressure),
+    //   btc: 5131.00,
+    //   eth: 156.57,
+    //   ltc: 72.70,
+    //   xlm: 0.97,
+    //   vet: 0.75
+
+    // });
+    this.setState({
+      btc: data.data[0],
     });
+
+  });
+
 }
 
   render() {
@@ -52,10 +78,6 @@ componentDidMount() {
         </div>
         <div className="Button">
             <button onClick={() => this.doFetch()}> Update Coin Data </button>
-
-            <button onClick={this.incrementCounter} >Click me to increment counter</button>
-
-            <button onClick={this.incrementDogCounter} >Click this to increment dog count</button>
         </div>
         <section id="output">
         </section>
